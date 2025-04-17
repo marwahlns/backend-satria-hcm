c5c59d4913a695efc3bd4520d9f37894fbe0938a
marwahlns <marwahnisa1903@gmail.com>
2025-04-17 11:14:51 +0700

    Auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,10 +4,6 @@ import * as AuthController from "../controllers/Auth/AuthController";
 
 const router = express.Router();
 
-// router.post("/register", async (req: Request, res: Response) => {
-//   await AuthController.registerUser(req, res);
-// });
-
 router.post("/login", async (req: Request, res: Response) => {
   await AuthController.loginUser(req, res);
 });
